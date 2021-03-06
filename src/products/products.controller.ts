5e0d95c6cfa7fca54003113512b6fcd860171625
Aziz Kudaikulov <aziz.kudaikulov@gmail.com>
2021-03-06 20:49:53 +0600

    Initial commit

---
+++ src/products/products.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { ProductsService } from './products.service';
+import { Product } from './schemas/product.schema';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {
+
+  }
+
+  @Get()
+  getAll(): Promise<Product[]> {
+    return this.productsService.getAll();
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: string): Promise<Product> {
+    return this.productsService.getById(id);
+  }
+
+  @Post()
+  create(@Body() createProduct: CreateProductDto): Promise<Product> {
+    return this.productsService.create(createProduct);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<Product> {
+    return this.productsService.remove(id);
+  }
+
+  @Put(':id')
+  update(@Body() updateProduct: UpdateProductDto, @Param('id') id: string): Promise<Product> {
+    return this.productsService.update(id, updateProduct);
+  }
+}
